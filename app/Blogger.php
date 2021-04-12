7d8464f2bbc9240856cf83ecd424d7eb3bc70ea0
Iryeri <sof.rojram@gmail.com>
2021-04-12 10:10:21 -0600

    Tabla, Modelo y Controlador

---
+++ app/Blogger.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Blogger extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'blogger';
+
+    protected $fillable = 
+    [
+        'name',
+        'email',
+        'password'
+    ];
+
+    protected $hidden =
+    [
+        'password',
+        'remember_token'
+    ];
+}
